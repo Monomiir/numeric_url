1ac6d2d1533a590ca744ca6c58e58896c1d9b97c
Monomiir <49772244+Monomiir@users.noreply.github.com>
2025-06-20 21:57:17 +0900

    Update plugin.php
    URI changed

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/Monomiir/numeric_url
 Description: Generate YOURLS short URLs with numbers only and customizable length.
 Version: 1.1
 Author: Monomiir
-Author URI: https://2900.org
+Author URI: https://7100.org
 */
 
 // Hook into the keyword generation process
